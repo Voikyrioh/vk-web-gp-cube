4260bc3f897c371e86101a7332c4a90a2c40bbfd
Voikyrioh <voikyrioh@gmail.com>
2024-08-22 22:01:55 +0200

    Updated Tests for Matrix 4x4

--- src/app/core/class/__tests__/VacMatric.spec.ts
+++ src/app/core/class/__tests__/VacMatric.spec.ts
@@ -6,55 +6,62 @@ describe('[unit] VecMatrix Class', () => {
         it('Should create VecMatrix from MatrixArray', () => {
             expect(VecMatrix).to.be.a('function');
             const myMatrix = new VecMatrix([
-                2,3,1,
-                0,4,3,
-                1,4,2
+                1,   2,  3,  4,
+                5,   6,  7,  8,
+                9,  10, 11, 12,
+                13, 14, 15, 16
             ]);
             expect(myMatrix).to.not.be.undefined;
             expect(myMatrix.getMatrix()).to.be.a('array').that.deep.equal([
-                2,3,1,
-                0,4,3,
-                1,4,2
+                1,   2,  3,  4,
+                5,   6,  7,  8,
+                9,  10, 11, 12,
+                13, 14, 15, 16
             ]);
         })
 
         it('Should set correct values into rows and columns', () => {
             const myMatrix = new VecMatrix([
-                1,2,3,
-                4,5,6,
-                7,8,9
+                1,   2,  3,  4,
+                5,   6,  7,  8,
+                9,  10, 11, 12,
+                13, 14, 15, 16
             ]);
 
-            expect(myMatrix.rows).to.have.lengthOf(3).and.to.be.deep.equal([
-                [1,2,3],
-                [4,5,6],
-                [7,8,9],
+            expect(myMatrix.rows).to.have.lengthOf(4).and.to.be.deep.equal([
+                [1,   2,  3,  4],
+                [5,   6,  7,  8],
+                [9,  10, 11, 12],
+                [13, 14, 15, 16]
             ]);
 
-            expect(myMatrix.columns).to.have.lengthOf(3).and.to.be.deep.equal([
-                [1,4,7],
-                [2,5,8],
-                [3,6,9],
+            expect(myMatrix.columns).to.have.lengthOf(4).and.to.be.deep.equal([
+                [ 1, 5,  9, 13 ],
+                [ 2, 6, 10, 14 ],
+                [ 3, 7, 11, 15 ],
+                [ 4, 8, 12, 16 ]
             ]);
         })
     });
 
     describe('Multiply()', () => {
         it('should multiply correctly two MatrixArrays', () => {
-            expect(VecMatrix.multiply(
-                [
-                    2,3,1,
-                    0,4,3,
-                    1,4,2
+            expect(VecMatrix.multiply([
+                    1,   2,  3,  4,
+                    5,   6,  7,  8,
+                    9,  10, 11, 12,
+                    13, 14, 15, 16
                 ],[
-                    1,0,2,
-                    0,1,3,
-                    3,4,3
+                    16, 15, 14, 13,
+                    12, 11, 10,  9,
+                    8,  7,  6,  5,
+                    4,  3,  2,  1
                 ])
             ).to.be.a('array').that.deep.equal([
-                    4,11,5,
-                    3,16,9,
-                    9,37,21
+                386, 444, 502, 560,
+                274, 316, 358, 400,
+                162, 188, 214, 240,
+                50, 60, 70, 80
             ]);
         });
     });
